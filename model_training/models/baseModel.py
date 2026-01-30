f26eab2b55f797b76c67a6e25af72602592b2cae
db222wf <db222wf@student.lnu.se>
2026-01-30 13:27:02 +0100

    new output later

--- model_training/models/baseModel.py
+++ model_training/models/baseModel.py
@@ -1,16 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras import layers, Model
 
 
 class BaseModel:
-    def __init__(self, model, input_shape=(224, 224, 3), classes=1000, weights='imagenet', classifier_activation='softmax'):
+    def __init__(self, model, input_shape=(224, 224, 3), weights='imagenet', classifier_activation='softmax', dense_units=256, output_activation='sigmoid'):
         self.model = model(
                 include_top=False,
                 weights=weights,
                 input_tensor=None,
                 input_shape=input_shape,
                 pooling=None,
-                classes=classes,
                 classifier_activation=classifier_activation
         )
+        outputlayer = self.model.output
+        outputlayer = layers.GlobalAveragePooling2D()(outputlayer)
+        outputlayer = layers.Dense(dense_units, activation=classifier_activation)(outputlayer)
+        outputs = layers.Dense(1, activation=output_activation)(outputlayer)
+        self.model = Model(inputs=self.model.input, outputs=outputs)
+
+
+
+
+
+
 
     def train(self, data, labels, epochs=10, batch_size=32, optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy']):
         self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
