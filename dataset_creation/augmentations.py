1dc9e13660fc6ad67634a2fdba9429a26a70a220
db222wf <db222wf@student.lnu.se>
2026-02-03 13:32:20 +0100

    testing augmentation code

--- dataset_creation/augmentations.py
+++ dataset_creation/augmentations.py
@@ -44,4 +44,13 @@ def augment_rand_once(image): # if image is path to image file, then uncomment c
     # image = tf.image.decode_jpeg(image, channels=3)
     # image = tf.image.convert_image_dtype(image, tf.float32)
     data_augmentation = tf.keras.Sequential([ layers.RandomFlip("horizontal_and_vertical"), layers.RandomRotation(0.028) ])
-    return data_augmentation(image)
+    return data_augmentation(image, training=True)
+
+
+if __name__ == "__main__":
+    image = tf.io.read_file("C:\\Programmering\\Masters\\DL-Mechanical-Defects\\dataset_creation\\processed_images\\initial\\bad\\1\\8.jpg")
+    image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+
+    augmented = augment_rand_once(image)
+    tf.keras.utils.save_img("C:\\Programmering\\Masters\\DL-Mechanical-Defects\\dataset_creation\\augmented1.jpg", augmented)
