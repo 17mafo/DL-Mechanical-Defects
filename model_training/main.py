f947392eb9708044da3f460b4d2d4d94a513cada
Martin <martin.fontin@hotmail.com>
2026-02-03 11:12:43 +0100

    New pipline model creation,

--- model_training/main.py
+++ model_training/main.py
@@ -1,16 +1,33 @@
 from ml_pipeline import MLPipeline
 from tensorflow.keras.applications import ResNet50
+# preprocess input function for ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input as ResNet50_preprocess_input
 
 def main():
     path_to_data = "C:\\Users\\marti\\Documents\\DL-Mechanical-Defects\\dataset_creation"
     pipeline = MLPipeline(path_to_data)
 
     # Example of adding a model to the pipeline
-    path = "C:\\Users\\marti\\Documents\\DL-Mechanical-Defects\\dataset_creation"
-    pipeline.add_model(ResNet50, path, focusmodels=True, bad_but_looks_good_equals_bad=None, params={'dense_units': 512})
-    
-    # pipeline.print_models()
-    pipeline.run_pipline()
+
+    # params include (Default values):
+    # dense_units : 32
+    # data_limit : 32
+    # batch_size : 32
+    # img_size : (300,300)
+    # val_split : 0.2
+    # epochs : 10
+
+    pipeline.add_model(ResNet50, 
+                       image_type=["initial"],                       
+                       focus=["1", "2"],
+                       preprocess_input=ResNet50_preprocess_input,
+                       params={'dense_units': 512,
+                               'data_limit':32,
+                               'epochs':50,
+                               'batch_size':32,})
+
+    pipeline.print_models()
+    # pipeline.run_pipeline()
 
 if __name__ == "__main__":
     main()
--- model_training/ml_pipeline.py
+++ model_training/ml_pipeline.py
@@ -8,106 +8,34 @@ class MLPipeline:
         self.path_to_data = path_to_data
         self.models = []
 
-    
-    def add_model(self, model, path, focusmodels = True, bad_but_looks_good_equals_bad = None, **params):
-        # This code is not understandable and creates all the needed models depending on the "focusmodels and bad_but_looks_good_equals_bad", Good luck :)
-        def create_paths(path, bad_but_looks_good_equals_bad, image_type):
-            good_paths = []
-            bad_paths = []
-            good_paths.append(path + f"/processed_images/{image_type}/good")
-            bad_paths.append(path + f"/processed_images/{image_type}/bad")
-            if(bad_but_looks_good_equals_bad):
-                bad_paths.append(path + f"/processed_images/{image_type}/bad_but_looks_good")
-            else:
-                good_paths.append(path + f"/processed_images/{image_type}/bad_but_looks_good")
-
-            return good_paths, bad_paths
-        
-        def redundant_loops(good_path, bad_path):
-            all_good_focus1 = good_path.copy()
-            all_bad_focus1 = bad_path.copy()
-            all_good_focus1 = [p + "/1" for p in good_path]
-            all_bad_focus1  = [p + "/1" for p in bad_path]
-
-            all_good_focus2 = good_path.copy()
-            all_bad_focus2 = bad_path.copy()
-            all_good_focus2 = [p + "/2" for p in good_path]
-            all_bad_focus2  = [p + "/2" for p in bad_path]
-            return all_good_focus1, all_bad_focus1, all_good_focus2, all_bad_focus2
-
-        for image_type in (["background", "initial", "outer_rim"]):
-            temp_focusModel = []
-            if(bad_but_looks_good_equals_bad is None):
-                # Model 1
-                good_paths, bad_paths = create_paths(path, False, image_type)
-                temp_focusModel.append([good_paths, bad_paths])
-
-                # Model 2
-                good_paths2, bad_paths2 = create_paths(path, True, image_type)
-                temp_focusModel.append([good_paths2, bad_paths2])
-
-            elif(bad_but_looks_good_equals_bad):
-                good_paths, bad_paths = create_paths(path, True, image_type)
-                temp_focusModel.append([good_paths, bad_paths])
-            else:
-                good_paths, bad_paths = create_paths(path, False, image_type)
-                temp_focusModel.append([good_paths, bad_paths])
-
-            for good_path, bad_path in temp_focusModel:
-                if(focusmodels is None):
-                    all_good_focus1, all_bad_focus1, all_good_focus2, all_bad_focus2 = redundant_loops(good_path, bad_path)
-
-
-                    # Model 1
-                    train_ds, val_ds = self.create_datasets(all_good_focus1, all_bad_focus1)
-                    self.models.append([model, params, train_ds, val_ds, all_good_focus1, all_bad_focus1])
-                    # Model 2
-                    train_ds, val_ds = self.create_datasets(all_good_focus2, all_bad_focus2)
-                    self.models.append([model, params, train_ds, val_ds, all_good_focus2, all_bad_focus2])
+    def add_model(self, model, image_type, focus, preprocess_input, **params):
+        # Create model instances for each image_type and focus combination
+        for type in image_type:
+            for f in focus:
+                good_paths = [self.path_to_data + f"/processed_images/{type}/good/{f}"]
+                bad_paths = [self.path_to_data + f"/processed_images/{type}/bad/{f}"]
                 
-                    all_good_focus1, all_bad_focus1, all_good_focus2, all_bad_focus2 = redundant_loops(good_path, bad_path)
-                    
-                    # Model 1
-                    all_good_focus1.extend(all_good_focus2)
-                    all_bad_focus1.extend(all_bad_focus2)
-                    train_ds, val_ds = self.create_datasets(all_good_focus1, all_bad_focus1)
-                    self.models.append([model, params, train_ds, val_ds, all_good_focus1, all_bad_focus1])
+                train_ds, val_ds = self.create_datasets(good_paths, bad_paths,
+                                                        batch_size = params.get('batch_size', 32),
+                                                        img_size = params.get('img_size', (300,300)),
+                                                        val_split= params.get('val_split', 0.2),
+                                                        data_limit= params.get('data_limit', 500))
+                self.models.append([f"{model().name}_{type}_{f}",model, params, train_ds, val_ds, preprocess_input])
 
-                elif(focusmodels):
-                    all_good_focus1, all_bad_focus1, all_good_focus2, all_bad_focus2 = redundant_loops(good_path, bad_path)
-
-                    # Model 1
-                    train_ds, val_ds = self.create_datasets(all_good_focus1, all_bad_focus1)
-                    self.models.append([model, params, train_ds, val_ds, all_good_focus1, all_bad_focus1])
-                    # Model 2
-                    train_ds, val_ds = self.create_datasets(all_good_focus2, all_bad_focus2)
-                    self.models.append([model, params, train_ds, val_ds, all_good_focus2, all_bad_focus2])
-                else:
-                    all_good_focus1, all_bad_focus1, all_good_focus2, all_bad_focus2 = redundant_loops(good_path, bad_path)
-                    
-                    # Model 1
-                    all_good_focus1.extend(all_good_focus2)
-                    all_bad_focus1.extend(all_bad_focus2)
-                    train_ds, val_ds = self.create_datasets(all_good_focus1, all_bad_focus1)
-                    self.models.append([model, params, train_ds, val_ds, all_good_focus1, all_bad_focus1])
-
-                
     def print_models(self):
         print("Current models in the pipeline:")
         for i, model in enumerate(self.models):
             print(f"Model {i+1}:")
-            print(f"  Model Type: {model[0]}")
-            print(f"  Parameters: {model[1]}")
-            print(f"  Training Dataset: {model[2]}")
-            print(f"  Validation Dataset: {model[3]}")
+            print(f"  Model Name: {model[0]}")
+            print(f"  Parameters: {model[2]}")
+            print(f"  Training Dataset: {model[3]}")
+            print(f"  Validation Dataset: {model[4]}")
             print(model)
 
-
-
-    def create_datasets(self, good_paths, bad_paths, BATCH_SIZE=32, IMG_SIZE=(300,300), VAL_SPLIT=0.2, DATA_LIMIT=500):
+    def create_datasets(self, good_paths, bad_paths, batch_size=32, img_size=(300,300), val_split=0.2, data_limit=500):
         # Load GOOD images (label = 0)
         for i, path in enumerate(good_paths):
-            good_ds_part = self.get_data(path, BATCH_SIZE, IMG_SIZE)
+            good_ds_part = self.get_data(path, batch_size, img_size)
             good_ds_part = good_ds_part.map(
                 lambda x: (x, tf.zeros((tf.shape(x)[0], 1))),
                 num_parallel_calls=tf.data.AUTOTUNE
@@ -119,7 +47,7 @@ class MLPipeline:
 
         # Load BAD images (label = 1)
         for i, path in enumerate(bad_paths):
-            bad_ds_part = self.get_data(path, BATCH_SIZE, IMG_SIZE)
+            bad_ds_part = self.get_data(path, batch_size, img_size)
             bad_ds_part = bad_ds_part.map(
                 lambda x: (x, tf.ones((tf.shape(x)[0], 1))),
                 num_parallel_calls=tf.data.AUTOTUNE
@@ -130,8 +58,8 @@ class MLPipeline:
                 bad_ds = bad_ds.concatenate(bad_ds_part)
         
         # Limit dataset size for testing
-        good_ds = good_ds.take(DATA_LIMIT // BATCH_SIZE)
-        bad_ds = bad_ds.take(DATA_LIMIT // BATCH_SIZE)
+        good_ds = good_ds.take(data_limit // batch_size)
+        bad_ds = bad_ds.take(data_limit // batch_size)
 
         # Combine datasets
         dataset = good_ds.concatenate(bad_ds)
@@ -139,7 +67,7 @@ class MLPipeline:
 
         # Train / validation split
         dataset_size = tf.data.experimental.cardinality(dataset).numpy()
-        val_size = int(dataset_size * VAL_SPLIT)
+        val_size = int(dataset_size * val_split)
 
         val_ds = dataset.take(val_size)
         train_ds = dataset.skip(val_size)
@@ -154,11 +82,11 @@ class MLPipeline:
             shuffle=True
         )
 
-    def run_pipline(self):
+    def run_pipeline(self):
         for model in self.models:
-            mod = bm(model[0], model[1])
-            mod.preprocess(model[2], model[3], self.preprocess_input)
-            mod.train(model[1])
+            mod = bm(model[0], **model[1])
+            mod.preprocess(model[2], model[3], model[4])
+            mod.train(**model[1])
             mod.summary()
 
 
